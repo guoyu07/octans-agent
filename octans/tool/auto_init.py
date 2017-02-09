55155e00ea5086447bbfe4c460872d7e2b803ea6
PengTao <pengtao@staff.weibo.com>
2017-02-09 18:45:43 +0800

    fix something

--- octans/tool/auto_init.py
+++ octans/tool/auto_init.py
@@ -19,19 +19,20 @@ def retry(attempt):
                     time.sleep(1)
         return wrapper
     return decorator
+
 @retry(attempt=1000)
 def run_task_over():
-	import uuid
+    import uuid
 #    import pdb;pdb.set_trace()
     payload = {"node":myaddr,
         "tasks":["init"],
         "tasktype": "ansible_role",
         "user": "root",
-	    "name":myaddr+myaddr+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-	    "fork_num": 5}
+        "name":myaddr+myaddr+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        "fork_num": 5}
     headers ={'content-type': 'application/json',
-	          'X-CORRELATION-ID': str(uuid.uuid1()),
-	          'X-SOURCE':'jupiter'
+              'X-CORRELATION-ID': str(uuid.uuid1()),
+              'X-SOURCE':'jupiter'
               'Cache-Control':'no-cache'}
     r = requests.post("http://127.0.0.1:8000/task/run",data=json.dumps(payload), headers=headers)
     if r.json().has_key("status"):
