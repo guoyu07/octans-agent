532c1b59e9a94f0e6c8558563e5dc7caefce6855
PengTao <pengtao@staff.weibo.com>
2017-02-10 15:55:21 +0800

    fix something

--- octans/tool/auto_init.py
+++ octans/tool/auto_init.py
@@ -7,6 +7,17 @@ import uuid
 from sys import argv
 myname = socket.getfqdn(socket.gethostname())
 myaddr = socket.gethostbyname(myname)
+requests.adapters.DEFAULT_RETRIES = 5
+s = requests.session()
+s.keep_alive = False
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='/root/myapp.log',
+                filemode='w')
 
 def retry(attempt):
     def decorator(func):
@@ -38,9 +49,11 @@ def check_task_status(task_id):
                     return "success"
     raise Exception("Run task's Exception raised!")
 
+@retry(attempt=10)
 def run_task_over():
     import uuid
 #    import pdb;pdb.set_trace()
+    logging.info('This is info message %s' %(argv[1]))
     payload = {"nodes":[argv[1]],
         "tasks":["init"],
         "tasktype": "ansible_role",
@@ -52,13 +65,17 @@ def run_task_over():
               'X-SOURCE':'jupiter',
               'Cache-Control':'no-cache'}
     r = requests.post("http://127.0.0.1:8000/api/run",data=json.dumps(payload), headers=headers)
+    logging.info('This is result %s' %(r.json()))
     task_id = 123
     if r.json().has_key("content"):
         if  r.json()["content"].has_key("id"):
             print(r.json()['content']['id'])
             task_id = r.json()['content']['id']
+            time.sleep(10)
             task_status = check_task_status(task_id)
             if task_status == "success":
                 return
     raise Exception("Run task's Exception raised!")
+
 run_task_over()
+
